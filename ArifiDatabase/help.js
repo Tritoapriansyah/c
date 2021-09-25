2ace7788645dc4235bb730e62663fad4eb21b2eb
Tritoapriansyah <riantrito@gmail.com>
2021-09-25 22:20:53 +0700

    p

---
+++ ArifiDatabase/help.js
@@ -0,0 +1,282 @@
+
+const help = (pushname, prefix, getLevelingLevel, getLevelingXp, premi, sender, reqXp, _registered, uangku) => { 
+	return `     
+
+*Selamat ${ucapan.data.result} I'm ${NamaBot}*
+╔════□ *Info Developer* □
+║╔▸ 
+║╠➤ *Nama* : ManikGanz
+║╠➤ *Nomer* : wa.me/6285737134572
+║╠➤ *Github* : Cari Sendiri!?
+║╠➤ *Tag Owner* : @${nik.split("@")[0]}
+║╠➤ *Yutub* : 「 ᴍᴀɴɪᴋ ᴏғғɪᴄɪᴀʟツ 」
+║╠➤ *Ig* : @Aryamanik___
+║╠➤ *Ulang Tahun Owner:V*
+║╠➤ *${ultah.data.result}*
+║╚▸
+╠════□ *Info User* □
+║╔▸ 
+║╠➤ *Nama* : ${pushname}
+║╠➤ *Nomer* : @${sender.split("@")[0]}
+║╠➤ *Uang mu* : Rp${uangku}
+║╠➤ *Premium* : ${premi}
+║╠➤ *XP* : ${getLevelingXp(sender)}/${reqXp}
+║╠➤ *Level* : ${getLevelingLevel(sender)}
+║╠➤ *User register* : ${_registered.length}
+║╚▸
+╠════□ *Info ${NamaNot}* □
+║╔▸ 
+║╠➤ *Name* : ${NamaBot}
+║╠➤ *Prefix* : ${prefix}
+║╠➤ *Speed* : ${process.uptime()}
+║╠➤ *Runtime* : ${kyun(uptime)}
+║╠➤ *Baterai :* ${battre}%
+║╠➤ *🕰Jam* : ${jam}:${menit}:${detik} ${ampm}
+║╠➤ *🗓️Tanggal* : ${day} - ${myMonths[bulan]} - ${year}
+║╠➤ *Versi Whatsapp* : ${manik.user.phone.wa_version}
+║╠➤ *Group Chat* : ${good.length}
+║╠➤ *Personal Chat*: ${totalchat.length - good.length}
+║╠➤ *Total Chat* : ${totalchat.length}
+║╚▸
+╠════□ *Other* □
+║╔▸ 
+║╠➤ *Grup Wa : ${prefix}manikgroup*
+║╠➤ *Sewabot? : ${prefix}sewabot*
+║╠➤ *Bug? : ${prefix}bugreport*
+║╠➤ *Req? : ${prefix}request*
+║╠➤ *Surat? : ${prefix}suratto <628xxx|Surat>*
+║╚▸
+╠════□ *Menu ${NamaBot}* □
+║╔▸ *◪ OWNER MENU ◪*
+║╠❏ ${prefix}antidelete aktif/mati(Group)
+║╠❏ ${prefix}antidelete ctaktif/ctmati (Cp)
+║╠❏ ${prefix}antidelete banct (Cp)
+║╠❏ ${prefix}attack gc (BugGc)
+║╠❏ ${prefix}addlimit
+║╠❏ ${prefix}setlimit
+║╠❏ ${prefix}setmemlimit
+║╠❏ ${prefix}addbadword
+║╠❏ ${prefix}dellbadword
+║╠❏ ${prefix}clearall
+║╠❏ ${prefix}setprefix
+║╠❏ ${prefix}bc
+║╠❏ ${prefix}event 1/0
+║╠❏ ${prefix}block
+║╠❏ ${prefix}unblock
+║╠❏ ${prefix}ban
+║╠❏ ${prefix}unban
+║╠❏ ${prefix}upswtext
+║╠❏ ${prefix}upswimg
+║╠❏ ${prefix}upswvid
+║╠❏ ${prefix}sendkontak
+║╠❏ ${prefix}sendkontag
+║╠❏ ${prefix}spamchat 
+║╠❏ ${prefix}term
+║╠❏ ${prefix}setname
+║╠❏ ${prefix}setbio
+║╠❏ ${prefix}fdeface
+║╠❏ ${prefix}spam
+║╠❏ ${prefix}imgtag
+║╠❏ ${prefix}stctag
+║╠❏ ${prefix}toimg
+║╠❏ ${prefix}togif
+║╠❏ ${prefix}tovideo
+║╠❏ ${prefix}grouplist
+║╠❏ ${prefix}autosticker (1/0)
+║╠❏ ${prefix}ncheck (1/0)
+║╠❏ ${prefix}fordward
+║╠❏ ${prefix}wame
+║╠❏ ${prefix}nobg
+┏━━□ *ANIME* □━━━┓
+┣□ *${prefix}anime*
+┣□ *${prefix}bokunh*
+┣□ *${prefix}sao*
+┣□ *${prefix}lovelive*
+┣□ *${prefix}animesaran/animesaran2*
+┣□ *${prefix}loli*
+┣□ *${prefix}hsdxd*
+┣□ *${prefix}husbu*
+┣□ *${prefix}waifu*
+┣□ *${prefix}neko*
+┣□ *${prefix}chika*
+┣□ *${prefix}kurumi*
+┣□ *${prefix}kaneki*
+┣□ *${prefix}touka*
+┣□ *${prefix}aot*
+┣□ *${prefix}naruto*
+┣□ *${prefix}minato*
+┣□ *${prefix}sagiri*
+┣□ *${prefix}tokyoghoul*
+┗━━━━━━━━━━━━━━━━
+
+┏━━□ *CREATOR* □━━━┓
+┣□ *${prefix}comunism*
+┣□ *${prefix}trigger*
+┣□ *${prefix}rotate*
+┣□ *${prefix}delete*
+┣□ *${prefix}thuglife*
+┣□ *${prefix}tobecontinue*
+┣□ *${prefix}smoke* <teks>
+┣□ *${prefix}camuflage* <teks>
+┣□ *${prefix}stars* <teks>
+┣□ *${prefix}3dglowing* <teks>
+┣□ *${prefix}underwaterocean* <teks>
+┣□ *${prefix}wolfmetal* <teks>
+┣□ *${prefix}rainbowshine* <teks>
+┣□ *${prefix}gtavposter*
+┣□ *${prefix}pencil*
+┣□ *${prefix}pencildrawing*
+┣□ *${prefix}burneffect*
+┣□ *${prefix}crossgun*
+┣□ *${prefix}customwp*
+┣□ *${prefix}nightbeach*
+┣□ *${prefix}raindrop*
+┣□ *${prefix}3dlinephoto*
+┣□ *${prefix}stiker/sticker*
+┣□ *${prefix}tahta* <teks>
+┣□ *${prefix}woodblock* <text>
+┣□ *${prefix}darkneon* <text>
+┣□ *${prefix}tlight* <text>
+┣□ *${prefix}paper* <text>
+┣□ *${prefix}matrix* <teks>
+┣□ *${prefix}text3dbox* <teks>
+┣□ *${prefix}breakwall* <text>
+┣□ *${prefix}didin* <text|text>
+┣□ *${prefix}gtext* <text|text>
+┣□ *${prefix}pornhub* <text|text>
+┣□ *${prefix}bneon* <text>
+┣□ *${prefix}coffee* <text>
+┣□ *${prefix}logobp* <text>
+┣□ *${prefix}leavest* <text>
+┣□ *${prefix}shadowt* <text>
+┣□ *${prefix}nulis* <text>
+┣□ *${prefix}attp* <text>
+┣□ *${prefix}ppcouple*
+┣□ *${prefix}porncomen* <text>
+┣□ *${prefix}tts* <kode bahasa> <text>
+┗━━━━━━━━━━━━━━━━
+
+┏━━□ *FUN* □━━━┓
+┣□ *${prefix}memeindo*
+┣□ *${prefix}darkjoke*
+┣□ *${prefix}asupan*
+┣□ *${prefix}pantun*
+┣□ *${prefix}quotes*
+┣□ *${prefix}cerhor*
+┣□ *${prefix}gombal*
+┣□ *${prefix}bucin*
+┣□ *${prefix}jadian*
+┣□ *${prefix}watak* <nama>
+┣□ *${prefix}kapankah* <teks>
+┣□ *${prefix}bisakah* <teks>
+┣□ *${prefix}terpintar*
+┣□ *${prefix}terganteng*
+┣□ *${prefix}tercantik*
+┣□ *${prefix}tebakkata*
+┣□ *${prefix}family100*
+┣□ *${prefix}caklontong*
+┣□ *${prefix}tebakgambar*
+┗━━━━━━━━━━━━━━━━
+
+┏━━□ *ISLAM* □━━━┓
+┣□ *${prefix}sedekah*
+┣□ *${prefix}qmuslim*
+┣□ *${prefix}rdoadl*
+┣□ *${prefix}listsurah*
+┣□ *${prefix}quranaudio*
+┣□ *${prefix}kisahnabi* <nama nabi>
+┗━━━━━━━━━━━━━━━━
+
+┏━━□ *ANBOT-D* □━━━┓
+┣□ *${prefix}dompet*
+┣□ *${prefix}buylimit*
+┣□ *${prefix}transfer*
+┣□ *${prefix}limit*
+┣□ *${prefix}level*
+┣□ *${prefix}bahasa*
+┣□ *${prefix}qrcode*
+┗━━━━━━━━━━━━━━━━
+
+┏━━□ *ALL* □━━━┓
+┣□ *${prefix}namajawa*
+┣□ *${prefix}pinte* <text>
+┣□ *${prefix}lb/leaderboard*
+┣□ *${prefix}aesthetic*
+┣□ *${prefix}listsound*
+┣□ *${prefix}cecen*
+┣□ *${prefix}cogan*
+┣□ *${prefix}tumur* <nama>
+┣□ *${prefix}mutual*
+┣□ *${prefix}spamcall* <nomor>
+┣□ *${prefix}spamsms* <nomor>
+┣□ *${prefix}spamgmail* <gmail/email>
+┣□ *${prefix}artinama* <nama>
+┣□ *${prefix}brainly* <pertanyaan>
+┣□ *${prefix}next*
+┣□ *${prefix}mimpi* <nama mimpi>
+┣□ *${prefix}tomp3* <merubah video menjadi audio>
+┣□ *${prefix}toimg* <merubah stiker menjadi gambar>
+┣□ *${prefix}rate* <text>
+┣□ *${prefix}addbucin* <text>
+┣□ *${prefix}gantengcek* <nana>
+┣□ *${prefix}cantikcek* <nama>
+┣□ *${prefix}kucing*
+┣□ *${prefix}listbadword*
+┣□ *${prefix}lizard*
+┣□ *${prefix}lirik* <text>
+┣□ *${prefix}igstalk* <username>
+┣□ *${prefix}tiktokstalk* <username>
+┗━━━━━━━━━━━━━━━━
+
+┏━━□ *PREMIUM* □━━━┓
+┣□ *${prefix}ytmp3* <link video yt>
+┣□ *${prefix}ytmp4* <link video yt>
+┣□ *${prefix}ytplaymp3/ytplaymp4* <judul>
+┣□ *${prefix}joox* <judul lagu>
+┗━━━━━━━━━━━━━━━━
+
+┏━━□ *INFO* □━━━┓
+┣□ *${prefix}info*
+┣□ *${prefix}premiumlist*
+┣□ *${prefix}owner*
+┗━━━━━━━━━━━━━━━━
+
+┏━━□ *GROUP* □━━━┓
+┣□ *${prefix}demote*
+┣□ *${prefix}promote*
+┣□ *${prefix}kick*
+┣□ *${prefix}listadmin*
+┣□ *${prefix}add*
+┣□ *${prefix}tagall*
+┣□ *${prefix}linkgc*
+┣□ *${prefix}delete*
+┣□ *${prefix}setdesc*
+┣□ *${prefix}setname*
+┗━━━━━━━━━━━━━━━━
+
+┏━━□ *OWNER* □━━━┓
+┣□ *${prefix}setppbot*
+┣□ *${prefix}unblock*
+┣□ *${prefix}addbadword*
+┣□ *${prefix}block*
+┣□ *${prefix}addprem*
+┣□ *${prefix}dellprem*
+┣□ *${prefix}leave*
+┣□ *${prefix}clearall*
+┗━━━━━━━━━━━━━━━━
+
+┏━━□ *ON/OFF* □━━━┓
+┣□ *${prefix}antilink* [1/0]
+┣□ *${prefix}antivirtex* [1/0]
+┣□ *${prefix}leveling* [enable/disable]
+┣□ *${prefix}nsfw* <1/0>
+┣□ *${prefix}nobadword* <enable/disable>
+┣□ *${prefix}welcome* [1/0]
+┣□ *${prefix}event* <1/0>
+┗━━━━━━━━━━━━━━━━
+   ║▌│█║▌│ █║▌│█│║▌║
+   ║▌│█║▌│ █║▌│█│║▌║
+        
+      *「 OREKI BOT 」*`
+}
+exports.help = help
---
+++ ArifiSrc/rules.js
@@ -0,0 +1,21 @@
+const rules = (prefix) => { 
+	return `
+*RULES BAGI PENGGUNA ANBOT-D*
+
+━━━━━━━━━(PENTING)━━━━━━━━━
+🦏 JANGAN SPAM BOT
+🦏 JANGAN CALL/VC BOT JIKA TIDAK INGIN DI BLOCK
+🦏 JANGAN LUPA UNTUK SELALU TERSENYUM
+🦏 SAYA TIDAK PERNAH MEMAKSA ANDA BERDONASI, JIKA INGIN BERDONASI CHAT OWMER
+🦏 JANGAN LUPA UNTUK SELALU BERIBADAH, DAN INGATLAH SELALU TERSENYUM
+━━━━━━━━━━━━━━━━━━━━━━━━
+
+ketik ${prefix}menu
+untuk melihat menu ANBOT-D
+
+   ║▌│█║▌│ █║▌│█│║▌║
+   ║▌│█║▌│ █║▌│█│║▌║
+        
+      *「 ANBOT-D 」*`
+}
+exports.rules = rules
